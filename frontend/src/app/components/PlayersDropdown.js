fb8328678a23bf0de9cff78d2d34a287b9fa2445
ashrafkaram2002 <ashraf.medhat.karam@gmail.com>
2024-09-26 22:10:22 +0300

    .

--- frontend/src/app/components/PlayersDropdown.js
+++ frontend/src/app/components/PlayersDropdown.js
@@ -27,7 +27,6 @@ const PlayersDropdown = ({ players, onSelect, loading, onRemove }) => {
     if (!selectedPlayers.some(p => p._id === player._id)) {
       setSelectedPlayers(prev => [...prev, player]);
       setAvailablePlayers(prev => prev.filter(p => p._id !== player._id));
-      console.log(player.position)
       onSelect(player);
     }
   };
@@ -43,8 +42,12 @@ const PlayersDropdown = ({ players, onSelect, loading, onRemove }) => {
   const rightCount = selectedPlayers.filter(player => player.position === 'right').length;
 
   return (
-    <div style={{paddingLeft:"2rem", paddingRight:"2rem"}}>
-
+    <div style={{ paddingLeft: "2rem", paddingRight: "2rem" }}>
+      
+      <div className="info-text" style={{ marginBottom: '0.5rem', fontSize: '0.9rem', color: '#fff', marginTop:'-0.9rem', textAlign:'left' }}>
+        Click on the players you want to select.
+      </div>
+      
       <div>
         <input
           type="text"
@@ -52,38 +55,41 @@ const PlayersDropdown = ({ players, onSelect, loading, onRemove }) => {
           placeholder="Search by name"
           onChange={(e) => setSearchTerm(e.target.value)}
           className="search-input"
-          style={{width:"100%", marginLeft: '-0.2rem'}}
+          style={{ width: "100%", marginLeft: '-0.2rem' }}
         />
-  
+
         <div className='mini-title'>Available Players</div>
         <div className='dropdown-container'>
-          {loading?(<div className="flex justify-center items-center">
-         <div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin border-white mt-4"></div>
-      </div>):(<ul>
-            {availablePlayers.length > 0 ? (
-              availablePlayers.map(player => (
-                <li 
-                  key={player._id}
-                  title={`${player.position}`}
-                  className='dropdown-item'
-                  onClick={() => handleSelectPlayer(player)}
-                >
-                  <div style={{fontWeight:"bold", marginRight:"2rem"}}>{player.position=="left"?"L":"R"}</div>
-                  <div>{player.name}</div>
-                  <span style={{marginLeft:"2rem", fontWeight:"lighter"}}>{player.points} pts</span>
-                </li>
-              ))
-            ) : (
-              <li className='dropdown-item'>No players available</li>
-            )}
-          </ul>)}
-          
+          {loading ? (
+            <div className="flex justify-center items-center">
+              <div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin border-white mt-4"></div>
+            </div>
+          ) : (
+            <ul>
+              {availablePlayers.length > 0 ? (
+                availablePlayers.map(player => (
+                  <li
+                    key={player._id}
+                    title={`${player.position}`}
+                    className='dropdown-item'
+                    onClick={() => handleSelectPlayer(player)}
+                  >
+                    <div style={{ fontWeight: "bold", marginRight: "2rem" }}>{player.position === "left" ? "L" : "R"}</div>
+                    <div>{player.name}</div>
+                    <span style={{ marginLeft: "2rem", fontWeight: "lighter" }}>{player.points} pts</span>
+                  </li>
+                ))
+              ) : (
+                <li className='dropdown-item'>No players available</li>
+              )}
+            </ul>
+          )}
         </div>
       </div>
-      
-      <div className='mini-title' style={{marginTop:"1rem"}}>
+
+      <div className='mini-title' style={{ marginTop: "1rem" }}>
         Selected Players
-        <span  className='subtitle' style={{marginLeft:"2rem"}}>
+        <span className='subtitle' style={{ marginLeft: "2rem" }}>
           Left: {leftCount} | Right: {rightCount}
         </span>
       </div>
@@ -92,10 +98,9 @@ const PlayersDropdown = ({ players, onSelect, loading, onRemove }) => {
           {selectedPlayers.length > 0 ? (
             selectedPlayers.map(player => (
               <li key={player._id} title={`${player.position}`} className='dropdown-item'>
-                <div style={{fontWeight:"bold", marginRight:"2rem"}}>{player.position=="left"?"L":"R"} </div>
+                <div style={{ fontWeight: "bold", marginRight: "2rem" }}>{player.position === "left" ? "L" : "R"} </div>
                 <div>{player.name}</div>
-                  <span style={{marginLeft:"2rem", fontWeight:"lighter"}}>{player.points} pts</span>
-                
+                <span style={{ marginLeft: "2rem", fontWeight: "lighter" }}>{player.points} pts</span>
                 <button onClick={() => handleRemovePlayer(player)}>
                   <MdDelete className="delete-icon" />
                 </button>
